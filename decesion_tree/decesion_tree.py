0ddf2f2b936c5b09da86bd5f176a1461b45be36a
zhouyayun <389377048@qq.com>
2019-05-27 17:15:44 +0800

    delete select_feature

--- decesion_tree/decesion_tree.py
+++ decesion_tree/decesion_tree.py
@@ -43,7 +43,7 @@ class decesion_tree():
 			p += (float(data)/n)**2
 		return 1 - p
 
-	def create_tree(data,depth,select_feature):
+	def create_tree(data,depth):
 		'''结束条件：
 		1.树的深度达到用户指定的要求
 		2.叶子节点的样本数少于指定的阈值
@@ -65,23 +65,22 @@ class decesion_tree():
 		origin_gini = self.gini(data)
 		#需要减去最后一列，因为最后一列是label
 		for i in range(n-1):
-			if i not in select_feature:
-				feature = data[:,[i]]
+			feature = data[:,[i]]
 			#选择所有可能的分割点(遍历每一个feature所有可能的取值)
-				for value in np.unique(feature):
+			for value in np.unique(feature):
 				#通过value来将数据集划分为两部分
-					set1,set2 = self.divide_set(data,i,value,n)
+				set1,set2 = self.divide_set(data,i,value,n)
 				#计算其gini指数，如果大于默认值则保存下来
-					p = len(set1)/m
-					gini = p*self.gini(set1)+(1-p)*self.gini(set2)
-					if gini < best_gini :
-						best_gini = gini
-						best_sets = (set1,set2)
-						best_attribute = (i,value)
+				p = len(set1)/m
+				gini = p*self.gini(set1)+(1-p)*self.gini(set2)
+				if gini < best_gini :
+					best_gini = gini
+					best_sets = (set1,set2)
+					best_attribute = (i,value)
 		if abs(origin_gini,best_gini) > tol:
-			select_feature.append(best_attribute[0])
-			right = create_tree(set1,depth-1,select_feature)
-			left = create_tree(set2,depth-1,select_feature)
+			#select_feature.append(best_attribute[0])
+			right = self.create_tree(set1,depth-1)
+			left = self.create_tree(set2,depth-1)
 			return tree_node(col=best_attribute[0], value=best_attribute[1], right=right, left=left)
 		else:
 			return tree_node()
@@ -98,4 +97,5 @@ class decesion_tree():
 		data = np.concatenate((data, class_label), axis=1)
 		#select_feature表示已经作为分类的特征不能用于其他层的分类(这种做法不妥，比如特征Ａ,第一层的分类安装Ａ<10分为两类，第二层可以继续Ａ>3继续进行划分)
 		#所以不应该使用select_feature
-		self.create_tree(data,self.depth,select_feature=[])
+		#self.create_tree(data,self.depth,select_feature)
+		self.create_tree(data,self.depth)
